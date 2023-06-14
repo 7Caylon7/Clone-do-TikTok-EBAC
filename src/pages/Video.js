72e02bb63d8f48f0cedc966f1aa07a7deba2a6fc
7Caylon7 <caylon.costa@itec.ufpa.br>
2023-06-14 20:31:41 -0300

    Segunda aula do webnar, montagem inicial do front

---
+++ src/pages/Video.js
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from 'react'
+import "./video.css"
+function Video() {
+    const videoRef = useRef(null)
+    const [play, setPlay] = useState(false)
+    function handleStart(){ /*função que dá play ao clicar no vídeo*/
+        
+        if(play){
+            videoRef.current.pause()
+            setPlay(false)
+        } else {
+            videoRef.current.play()
+            setPlay(true)
+        }
+    }
+
+
+
+  return (
+    <div className='video'>
+        <video 
+            className='video_player'
+            ref={videoRef}
+            onClick={handleStart}
+            loop /*parametro pra fazer o vídeo se manter em loop*/
+            src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+            >
+        </video>
+    </div>
+  )
+}
+
+export default Video
