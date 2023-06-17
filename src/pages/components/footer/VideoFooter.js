65de2362ae7bb2f39701d7d46938d7f70b15e541
7Caylon7 <caylon.costa@itec.ufpa.br>
2023-06-17 12:31:38 -0300

    Finalizando o footer e a aula 3, consegui resolver o que tava em impedindo de instalar o material Icon

--- src/pages/components/footer/VideoFooter.js
+++ src/pages/components/footer/VideoFooter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./VideoFooter.css";
-import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import { MusicNote } from "@mui/icons-material";
 
 function VideoFooter() {
   return (
@@ -9,14 +9,16 @@ function VideoFooter() {
         <h3>@Caylon S.</h3>
         <p>Descrição do vídeo</p>
         <div className="videoFooter_music">
-          
-          <p>Título da música</p>
+          <MusicNote/>
+          <div className="videoFooterMusic_text">
+            <p>Título da música</p>
+          </div>
         </div>
       </div>
-      <img className="videoFooter_records" 
-      alt="Imagem de um vinil girando"
-      src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/vinil.png?alt=media&token=d174bbec-b930-41e4-ab50-af1ea81bb7da"
-
+      <img
+        className="videoFooter_records"
+        alt="Imagem de um vinil girando"
+        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/vinil.png?alt=media&token=d174bbec-b930-41e4-ab50-af1ea81bb7da"
       />
     </div>
   );
